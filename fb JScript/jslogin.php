e923b9e9e4e698361091dce947a65f8d8be0e0ee
str-sachin <82816857+str-sachin@users.noreply.github.com>
2021-07-16 19:24:41 +0530

    Add files via upload

---
+++ fb JScript/jslogin.php
@@ -0,0 +1,12 @@
+<!-- Display login status -->
+<div id="status"></div>
+
+<!-- Facebook login or logout button -->
+<a href="javascript:void(0);" onclick="fbLogin();" id="fbLink">Login For Facebook</a>
+
+<!-- Display user's profile info -->
+<div class="ac-data" id="userData"></div>
+
+<footer>
+    <?php include('footer.php'); ?>
+</footer>
